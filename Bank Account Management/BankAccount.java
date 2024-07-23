445e5de8890ef6b603f7659fbfc71076a021ae49
Abimash Mishra <112063351+171001@users.noreply.github.com>
2024-07-23 10:49:21 +0530

    Add files via upload

---
+++ Bank Account Management/BankAccount.java
@@ -0,0 +1,14 @@
+import java.io.Serializable;
+
+public class BankAccount implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+	private String accountNumber;
+	private String accountHolder;
+	private double balance;
+	
+	public BankAccount(String accountNumber, String accountHolder) {
+		
+	}
+
+}
